2a3b8504c6202cf02a8ab0b972077d47bcc0884f
yeonguk_jang <young1uk2@gmail.com>
2022-10-17 04:04:00 +0900

    #1 스프링부트 프로젝트 생성 및 테스트

---
+++ src/main/java/org/zerouk/boot/BootApplication.java
@@ -0,0 +1,13 @@
+package org.zerouk.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootApplication.class, args);
+    }
+
+}
