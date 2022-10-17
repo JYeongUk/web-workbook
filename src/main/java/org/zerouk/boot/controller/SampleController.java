48b87167165c74aec3db7af363992d6efe9599a7
yeonguk_jang <young1uk2@gmail.com>
2022-10-18 00:54:12 +0900

    #2 테스트 코드로 CRUD 및 Paging 처리 확인

--- src/main/java/org/zerouk/boot/controller/SampleController.java
+++ src/main/java/org/zerouk/boot/controller/SampleController.java
@@ -5,6 +5,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.Arrays;
+import java.util.List;
+
 @Controller
 @Log4j2
 public class SampleController {
@@ -14,4 +17,11 @@ public class SampleController {
         log.info("hello............");
         m.addAttribute("msg", "HELLO WORLD");
     }
+
+    @GetMapping("/ex/ex1")
+    public void ex1(Model m) {
+        List<String> list = Arrays.asList("AAA", "BBB", "CCC", "DDD");
+        m.addAttribute("list", list);
+    }
+
 }
